c7400be501e529497d4600949cae5e2bbbe13a47
josejosepreso <josehihat@gmail.com>
2025-12-07 20:44:54 -0600

    Initial commit

---
+++ src/main/java/com/indianpharma/catalog/IndianPharmaCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.indianpharma.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndianPharmaCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndianPharmaCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/indianpharma/catalog/IndianPharmaCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indianpharma.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndianPharmaCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
